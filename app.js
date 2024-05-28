c0f4bef7f06bf9057d48aaaac7c6757b54287527
Idder Ait El Mouden <139082259+IDDER29@users.noreply.github.com>
2024-05-28 10:14:54 +0100

    add joi to verify there a problem

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const uplodeImageRouter = require("./routes/uplodeImages");
+
 app.use(express.static("public"));
 app.use(uplodeImageRouter);
 
--- routes/uplodeImages.js
+++ routes/uplodeImages.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const uplodeImageRouter = express.Router();
+const Joi = require("joi");
 const upload = require("../middlewares/uplode-images");
 
-uplodeImageRouter.post("/upload", upload.single("Image"), (req, res) => {
-  res.send("Images téléchargées avec succès.");
+// Define Joi schema for validating the request body
+const imageSchema = Joi.object({
+  fieldname: Joi.required(),
+  originalname: Joi.required(),
+});
+
+uplodeImageRouter.post("/upload", upload.single("Image"), async (req, res) => {
+  console.log(req.body);
+  const { error } = await imageSchema.validate(req.file);
+  if (error) {
+    console.log("hi i'm an error");
+    return res.status(400).send(error.details[0].message);
+  } else {
+    res.send("Images uploaded successfully.");
+  }
 });
 
 module.exports = uplodeImageRouter;

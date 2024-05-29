e16750f8f044160e7dff1fada2ade82851e78ee8
Idder Ait El Mouden <139082259+IDDER29@users.noreply.github.com>
2024-05-29 12:37:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,16 @@
 const express = require("express");
 require("dotenv").config();
+// const routeUser=require('./routes/UserRoutes');
+// const routeAuth=require('./routes/AuthRoutes');
+const routes = require("./routes");
+
 const app = express();
-const bodyParser = require("body-parser");
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-const uplodeImageRouter = require("./routes/uplodeImages");
-const postRouter = require("./routes/PostRoutes");
-const likesRouter = require("./routes/LikesRoutes");
-const commentsRouter = require("./routes/CommentRoutes");
 
-app.use(express.static("public"));
-app.use(uplodeImageRouter);
-app.use(postRouter);
-app.use(likesRouter);
-app.use(commentsRouter);
+app.use("/api", routes.routeUser);
+app.use("/", routes.routAuth);
 
-app.use(bodyParser.json());
-app.use((err, req, res, next) => {
-  res.status(400).json(err.message);
-});
-/*
-app.use((err,req,res,next)=>{
-      err.statusCode = err.statusCode  500 
-      err.status = err.status  "error"
-      res.status(err.statusCode).json({
-          statusCode : err.statusCode,
-          status :err.status,
-          message:err.message,
-          stack : err.stack
-      })
-  })
-*/
-const port = process.env.APP_PORT || 8000;
+const port = process.env.APP_PORT;
 app.listen(port, () => {
   console.log(`Server is running on PORT ${port}....`);
 });

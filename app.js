e411c048c3ac76f840ca3af20e68b9435da44e93
Idder Ait El Mouden <139082259+IDDER29@users.noreply.github.com>
2024-05-29 12:38:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const express=require("express");
+const express = require("express");
 require("dotenv").config();
 // const routeUser=require('./routes/UserRoutes');
 // const routeAuth=require('./routes/AuthRoutes');
- const routes=require('./routes');
+const routes = require("./routes");
 
-const app=express();
+const app = express();
 app.use(express.json());
 
-app.use('/api',routes.routeUser);
-app.use('/',routes.routAuth);
+app.use("/api", routes.routeUser);
+app.use("/", routes.routAuth);
 
-const port=process.env.APP_PORT 
-app.listen(port,()=>{
- console.log(`Server is running on PORT ${port}....`)
-})
+const port = process.env.APP_PORT;
+app.listen(port, () => {
+  console.log(`Server is running on PORT ${port}....`);
+});
